ca649b8c4021e881aa48f87d63953de7a47f7aec
KimKiHong-1111 <rlghd0157@gmail.com>
2025-08-21 20:08:35 +0900

    feat: first settings

---
+++ src/main/java/api/jsboard/JsboardApplication.java
@@ -0,0 +1,13 @@
+package api.jsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsboardApplication.class, args);
+	}
+
+}
